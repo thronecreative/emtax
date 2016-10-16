8b85ff102f78aac8f65a08cadc6147c1a58f7d0c
aimhighagency <bryan@aimhighagency.com>
2016-10-16 15:55:45 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php include('partials/head.php'); ?>
+
+<div id="lines-top">
+<svg>
+	<defs>
+		<pattern id="pattern-stripe" width="6" height="80" patternUnits="userSpaceOnUse"patternTransform="rotate(45)">
+			<rect width="1" height="80" transform="translate(0,0)" fill="white"></rect>
+		</pattern>
+		<mask id="mask-stripe">
+			<rect x="0" y="0" width="100%" height="100%" fill="url(#pattern-stripe)" />
+		</mask>      
+	</defs>
+	<svg>
+	<rect class="hbar" x="0" y="0" width="100%" height="15"></rect>
+	</svg>
+</svg>
+
+</div>
+
+
+
+
+<div id="header" class="section">
+	<div class="section__inner">
+		<div class="logo-main"><img src="images/EM-logo.svg" alt=""></div>
+		<div class="tagline">
+			LOS ANGELES
+			<span class="cross"><img src="images/EM-x.svg" alt=""></span>
+			TAX SERVICES
+		</div>
+	</div>
+</div>
+
+<div id="what-we-do" class="section">
+	<div class="section__inner">
+		<h2 class="section__heading">we do this...</h2>
+		<a href="">Tax Preparation &amp; Planning</a>
+		<a href="">Accounting</a>
+		<a href="">Bookkeeping</a>
+		<a href="">Business Consulting</a>
+		<a href="">Payroll</a>
+		<a href="">IRS Tax Problems</a>
+		<a href="">Business Valuation</a>
+	</div>
+</div>
+
+<div id="you-can-do-this" class="section">
+	<div class="section__inner">
+		<h2 class="section__heading">...so you can do</h2>
+		<p>litteraly anything else</p>
+		<div class="arrow-down"><img src="images/EM-arrow-down.svg" alt=""></div>
+		<div class="images">
+			<div class="image"><img src="images/activities/ridge-hike.jpg" alt=""></div>
+			<div class="image"><img src="images/activities/surf.jpg" alt=""></div>
+			<div class="image"><img src="images/activities/violin.jpg" alt=""></div>
+			<div class="image"><img src="images/activities/water-jump.jpg" alt=""></div>
+		</div>
+	</div>
+</div>
+
+<div id="cta" class="section">
+	<div class="section__inner">
+		<h2 class="section__heading">
+			ready to enjoy your day?
+		</h2>
+		<div ><a href="" class="btn--big contact-show-btn">Let’s get started</a></div>
+	</div>
+</div>
+
+<div id="footer">
+	<div id="lines-bottom">
+		<svg><rect class="hbar" x="0" y="0" width="100%" height="80"></rect></svg>
+	</div>
+	<div class="copy">
+		&copy; 2016 E&amp;M Tax Services &mdash; Don't Steal. It's not nice.
+	</div>
+</div>
+
+<div id="contact-holder">
+	<div id="contact" class="section">
+		<div class="section__inner">
+			<h2 class="section__heading">Contact us today:</h2>
+			<div class="contact__email"><a href="mailto:info@emtaxservices.com">info@emtaxservices.com</a></div>
+			<div class="contact__phone">(323) 123-4567</div>
+			<a href="#" class="contact__close contact-hide-btn">close</a>
+		</div>
+	</div>
+</div>
+
+<?php include('partials/foot.php'); ?>
+
+		
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ partials/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+	<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title></title>
+		
+		<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,900i|Quicksand:300,400,700" rel="stylesheet">
+
+		<link rel="stylesheet" href="css/app.css">
+		
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<meta property="og:title" content="" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="" />
+		<meta property="og:image" content="" />
+	</head>
+	<body>
